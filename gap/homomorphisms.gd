2f7f319c77d962d642a50010a410dca70e167c88
sunnyquiver <sunnyquiver>
2010-11-05 13:37:35 +0000

    Added homomorphisms.gd, declaration file for homomorphisms of matrix modules.

---
+++ gap/homomorphisms.gd
@@ -0,0 +1,38 @@
+# GAP Declarations
+# $Id: homomorphisms.gd,v 1.1 2010/11/05 13:37:35 sunnyquiver Exp $
+
+DeclareCategory("IsPathAlgebraMatModuleMap", IsAdditiveElementWithZero and IsGeneralMapping and RespectsScalarMultiplication and IsTotal and IsSingleValued ); 
+DeclareCategoryFamily(  "IsPathAlgebraMatModuleMap" );
+DeclareCategoryCollections( "IsPathAlgebraMatModuleMap" );
+DeclareRepresentation("IsPathAlgebraMatModuleMapRep", IsComponentObjectRep and IsAttributeStoringRep, ["maps"]);
+
+DeclareOperation( "RightModuleHomOverPathAlgebra", [IsPathAlgebraMatModule, IsPathAlgebraMatModule, IsList] ); 
+DeclareAttribute( "PathAlgebraOfMatModuleMap", IsPathAlgebraMatModuleMap );
+DeclareOperation( "HomOverPathAlgebra", [IsPathAlgebraMatModule, IsPathAlgebraMatModule ] ); 
+DeclareOperation( "EndOverPathAlgebra", [IsPathAlgebraMatModule ] ); 
+DeclareOperation( "NumberOfNonIsoDirSummands", [IsPathAlgebraMatModule ] ); 
+DeclareOperation( "LiftIdempotents", [IsPathAlgebraMatModule ] ); 
+DeclareOperation( "Ker", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "KerInclusion", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "ImProjectionInclusion", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "ImProjection", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "ImInclusion", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "Im", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "Coker", [IsPathAlgebraMatModuleMap ] ); 
+DeclareOperation( "CokerProjection", [IsPathAlgebraMatModuleMap ] ); 
+DeclareAttribute( "KernelOfWhat", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "CoKernelOfWhat", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "ImageOfWhat", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "IsOneToOne", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "IsOnto", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "IsIsom", IsPathAlgebraMatModuleMap );
+DeclareAttribute( "IsZeroMap", IsPathAlgebraMatModuleMap );
+DeclareOperation( "ZeroMap", [IsPathAlgebraMatModule, IsPathAlgebraMatModule] );
+DeclareOperation( "SubRep", [IsPathAlgebraMatModule, IsCollection]);
+DeclareOperation( "SubRepInclusion", [IsPathAlgebraMatModule, IsCollection]);
+DeclareOperation( "RadicalOfRep", [IsPathAlgebraMatModule]);
+DeclareOperation( "RadicalOfRepInclusion", [IsPathAlgebraMatModule]);
+DeclareOperation( "TopOfRep", [IsPathAlgebraMatModule]);
+DeclareOperation( "TopOfRepProjection", [IsPathAlgebraMatModule]);
+DeclareOperation( "GeneratorsOfRep", [IsPathAlgebraMatModule]);
+DeclareOperation( "RightFacApproximation", [IsPathAlgebraMatModule, IsPathAlgebraMatModule]);
