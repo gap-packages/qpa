0d12c23bfbdf75e1f6bc3e6a82860ea5d6814866
Øyvind Solberg <oyvinso@euler.math.ntnu.no>
2012-10-19 10:19:45 +0200

    Deleted unused gap files.

--- gap/alggrp.gd
+++
@@ -1,8 +0,0 @@
-# GAP Declarations
-# This file was generated from
-# $Id: alggrp.gd,v 1.1 2010/05/07 13:30:13 sunnyquiver Exp $
-DeclareSynonym( "IsGroupAlgebra", IsGroupRing and IsAlgebra);
-DeclareAttribute("GroupOfGroupAlgebra", IsGroupAlgebra );
-
-DeclareGlobalFunction( "GroupAlgebra" );
-
