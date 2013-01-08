03210153de445f77d9f7f34ebcc6d165f3a8a2b3
Andrzej Mroz <amroz@mat.umk.pl>
2013-01-08 15:32:43 +0100

    IsFiniteTypeAlgebra continued

---
+++ gap/pathalgreptype.gd
@@ -0,0 +1,6 @@
+# GAP Implementation
+# This contains various tools for determining a representation type of a (quotient of) path algebra
+# (created A. Mroz, 08.01.2013)
+
+DeclareProperty( "IsFiniteTypeAlgebra", IsPathAlgebra );
+DeclareProperty( "IsFiniteTypeAlgebra", IsQuotientOfPathAlgebra );
