efbcd4058aecd8a22b38845230ded61858848e3c
Oeyvind Solberg <oyvind.solberg@math.ntnu.no>
2015-07-20 12:37:55 +0200

    Removed the files doc/chap0.html, doc/manual.pdf and doc/manual.six from the git repository.  Changed the create-release.sh file.

--- create-release.sh
+++ create-release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-tarfile=qpa-version-$(cat version).tar
-git archive --prefix=qpa/ HEAD --format=tar > $tarfile
-gzip $tarfile 
+tarfile=qpa/qpa-version-$(cat version).tar
+cd ../
+tar -cvf $tarfile qpa/CHANGES qpa/LICENSE qpa/PackageInfo.g qpa/README qpa/create-release.sh qpa/doc qpa/examples qpa/init.g qpa/lib qpa/read.g qpa/tst qpa/version 
+gzip $tarfile
