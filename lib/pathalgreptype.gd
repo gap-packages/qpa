031c0aa725a3ada6e9789029627ba8bf5130d9f3
Oeyvind Solberg <oyvind.solberg@ntnu.no>
2020-04-28 15:00:14 +0200

    Fix bug in IsFiniteTypeAlgebra
    reported by Klaus Lux.

--- lib/pathalgreptype.gd
+++ lib/pathalgreptype.gd
@@ -2,6 +2,6 @@
 # This contains various tools for determining a representation type of a (quotient of) path algebra
 # (created A. Mroz, 08.01.2013)
 
-DeclareProperty( "IsFiniteTypeAlgebra", IsPathAlgebra );
-DeclareProperty( "IsFiniteTypeAlgebra", IsQuotientOfPathAlgebra );
+DeclareAttribute( "IsFiniteTypeAlgebra", IsPathAlgebra );
+DeclareAttribute( "IsFiniteTypeAlgebra", IsQuotientOfPathAlgebra );
 DeclareOperation(  "BongartzTest",  [ IsQuiverAlgebra, IS_INT ] );
