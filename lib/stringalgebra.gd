2046eb7d7b0321e9858917c4e15abd44b401ec7c
Bhargav Kale <kalebhargav@gmail.com>
2021-11-03 11:09:12 +0530

    Update stringalgebra.gd

--- lib/stringalgebra.gd
+++ lib/stringalgebra.gd
@@ -1,6 +1,6 @@
-DeclareOperation("IsABand", [ IsQuiver, IsList, IsString ]);
-DeclareOperation("StringsLessThan", [ IsQuiver, IsList, IsInt ]);
-DeclareOperation("BandsLessThan", [ IsQuiver, IsList, IsInt ]);
-DeclareOperation("BandRepresentativesLessThan", [ IsQuiver, IsList, IsInt ]);
-DeclareOperation("IsDomesticStringAlgebra", [ IsQuiver, IsList ]);
-DeclareOperation("BridgeQuiver", [ IsQuiver, IsList ]);
+DeclareOperation("IsABand", [ IsQuiverSA, IsList, IsString ]);
+DeclareOperation("StringsLessThan", [ IsQuiverSA, IsList, IsPosInt ]);
+DeclareOperation("BandsLessThan", [ IsQuiverSA, IsList, IsPosInt ]);
+DeclareOperation("BandRepresentativesLessThan", [ IsQuiverSA, IsList, IsPosInt ]);
+DeclareOperation("IsDomesticStringAlgebra", [ IsQuiverSA, IsList ]);
+DeclareOperation("BridgeQuiver", [ IsQuiverSA, IsList ]);
