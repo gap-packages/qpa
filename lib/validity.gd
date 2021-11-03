3df578a5d88c1e91c53c357e8bdba6f6d0c7ba92
Bhargav Kale <kalebhargav@gmail.com>
2021-11-03 11:05:39 +0530

    Update validity.gd

--- lib/validity.gd
+++ lib/validity.gd
@@ -1,2 +1,8 @@
-DeclareOperation("IsValidString", [ IsQuiver, IsList, IsString]);
-DeclareOperation("LocalARQuiver", [ IsQuiver, IsList, IsString ]);
+DeclareOperation("IsValidString", [ IsQuiverSA, IsList, IsString]);
+DeclareGlobalFunction( "QPAStringQuiverRho" );
+DeclareGlobalFunction( "QPAStringSigmaEps" );
+DeclareGlobalFunction( "QPAStringDirectLeft" );
+DeclareGlobalFunction( "QPAStringDirectRight" );
+DeclareGlobalFunction( "QPAStringInverseLeft" );
+DeclareGlobalFunction( "QPAStringInverseRight" );
+DeclareOperation("LocalARQuiver", [ IsQuiverSA, IsList, IsString ]);
