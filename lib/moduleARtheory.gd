39d77c1573972642cccbad64bbf138c2f97cc912
sunnyquiver <oyvind.solberg@ntnu.no>
2022-04-18 11:07:24 +0200

    Add functions IrreducibleMorphismsEndingIn and IrreducibleMorphismsStartingIn.

--- lib/moduleARtheory.gd
+++ lib/moduleARtheory.gd
@@ -1,3 +1,5 @@
 DeclareAttribute( "AlmostSplitSequence", IsPathAlgebraMatModule );
 DeclareOperation( "AlmostSplitSequenceInPerpT", [ IsPathAlgebraMatModule, IsPathAlgebraMatModule ] );
 DeclareOperation( "PredecessorsOfModule", [ IsPathAlgebraMatModule, IS_INT ]) ;
+DeclareAttribute( "IrreducibleMorphismsEndingIn", IsPathAlgebraMatModule );
+DeclareAttribute( "IrreducibleMorphismsStartingIn", IsPathAlgebraMatModule );
